9a808f61fb68ff13d8cd9dcf94092340f3d4cc7a
sainathmurali <sainath366@gmail.com>
2023-12-28 02:52:40 +0530

    Resources Page updated

--- hmnn-assets/gallery.js
+++ hmnn-assets/gallery.js
@@ -4,8 +4,11 @@ async function getData(url) {
         const data = await response.json();
         console.log(data);
 
-        for (let i = 0; i < data.galleryItems.length; i++) {
-            const item = data.galleryItems[i];
+        // Sort the gallery items based on the title (year) in descending order
+        const sortedGalleryItems = data.galleryItems.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+
+        for (let i = 0; i < sortedGalleryItems.length; i++) {
+            const item = sortedGalleryItems[i];
             // Handle the specific cell value separately, e.g., append it to a paragraph
             let galleryItem = createGalleryItem(item);
             $('#gallery-container').append(galleryItem);
@@ -20,7 +23,7 @@ function createGalleryItem(item) {
         <div class="col-sm-6 col-md-3 col-lg-3">
             <div class="gallery-item">
                 <div class="gallery-image">
-                    <a class="gallery" href="${item.ImagePath}" data-lightbox="gallery" data-title="${item.Title}">
+                    <a class="gallery" href="${item.ImagePath}" data-lightbox="gallery" data-title="${item.Year}">
                         <img src="${item.ThumbnailPath}" alt="Gallery Image"/>
                         <div class="gallery-caption">
                             <div class="gallery-icon"><span class="icon-magnifying-glass"></span></div>
